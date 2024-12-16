36b014aa65a39e4e55ec89950195cc309e4ca9ee
Fer_rosal <fernando.rosal@gmail.com>
2024-12-16 15:46:56 +0100

    Finished basic game!

--- pysteroids/main.py
+++ pysteroids/main.py
@@ -3,26 +3,59 @@
 # throughout this file
 import pygame
 from constants import *
-from circleshape import CircleShape
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from player import Player
+from shot import Shot
+
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    Player.containers = (drawable,updatable)
+    asteroidField = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        player.draw(screen)
+
+        for element in updatable:
+            element.update(dt)
+        
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
+        
+        for element in drawable:
+            element.draw(screen)
+
+        
         pygame.display.flip()
-        dt= clock.tick(60)
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
+
--- pysteroids/player.py
+++ pysteroids/player.py
@@ -1,11 +1,14 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
+    
     
     # in the player class
     def triangle(self):
@@ -18,3 +21,33 @@ class Player(CircleShape):
 
     def draw(self, p_screen):
         pygame.draw.polygon(p_screen,"white",self.triangle(),2)
+
+    def rotate(self,dt):
+        self.rotation += dt * PLAYER_TURN_SPEED
+    
+    def update(self, dt):
+        self.timer = self.timer - dt
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        if self.timer >0:
+            return
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+        self.timer = PLAYER_SHOT_COOLDOWN
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
+    
